23ee0ea8573cdb9bcd8732db6d744e3841ab90c5
wagnerfilh <wwfilho@live.com>
2022-10-23 22:14:53 -0300

    Revert "."
    This reverts commit d60cea377ded63d210c02ab5a0ff18a488eb14c3.

--- DB_txt/Armazenamento/salvando.py
+++
@@ -1,5 +0,0 @@
-import Interacao.inputs_usuario
-
-def memorinha():
-    with open(r'Armazenamento\dados_usuario.txt', 'a+') as arq:
-        arq.writelines(Interacao.inputs_usuario.interac()+'\n')
--- DB_txt/Interacao/inputs_usuario.py
+++
@@ -1,6 +0,0 @@
-def interac():
-    cpf = str(input("Digite o seu número de CPF: "))
-    nome = str(input("Digite o seu nome: "))
-    endereco = str(input("Digite o seu endereco: "))
-    dados = f"{cpf}\n{nome}\n{endereco}"
-    return dados
